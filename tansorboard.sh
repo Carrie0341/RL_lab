06d031c5ce26541c29941799dae7ba5ef53c0c33
Cary <asd0987576813@gmail.com>
2025-07-28 23:13:40 +0800

    fix: update log directory path in tensorboard script

--- tansorboard.sh
+++ tansorboard.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 設定基本日誌目錄
-BASE_LOG_DIR="logs/rl_games/Factory_Multi_Camera/visual_factory_multi_camera_v5_2"
+BASE_LOG_DIR="logs/rl_games/Factory_Multi_Camera/visual_factory_multi_camera_v5_3"
 
 # 檢查目錄是否存在
 if [ ! -d "$BASE_LOG_DIR" ]; then
